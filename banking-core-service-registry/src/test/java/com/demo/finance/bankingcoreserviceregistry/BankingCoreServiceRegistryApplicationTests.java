a6a14bad9b58bb7b74b1092c2f19f269a8bcc1a7
Janaka Wijerathne <js.wijerathne@gmail.com>
2023-12-17 22:40:48 +0530

    Added initial developments

---
+++ banking-core-service-registry/src/test/java/com/demo/finance/bankingcoreserviceregistry/BankingCoreServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.finance.bankingcoreserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingCoreServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
